5bbf14c95e23c3fdd68557d462396067fe13951a
vignesh-vic <vignesha010@gmail.com>
2026-03-19 22:29:59 +0530

    added express validation for auth routes

--- BE/routes/authRoutes.js
+++ BE/routes/authRoutes.js
@@ -3,6 +3,33 @@ import express from 'express'
 import { body } from 'express-validator'
 import {login,register,getProfile, updateProfile, changePassword} from '../controllers/authController'
 import protect from "../middleware/auth.js"
+import { body } from 'express-validator'
 
 const router = express.Router()
 
+const registerValidation = [
+    body('name').notEmpty().withMessage('Name is required'),
+    body('email').isEmail().normalizeEmail().withMessage('Invalid email format'),
+    body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 chars'),
+];
+ 
+
+const loginValidation = [
+    body("email")
+        .isEmail()
+        .normalizeEmail()
+        .withMessage("Please enter a valid email address"),
+    body("password")
+        .notEmpty()
+        .withMessage("Password is required")
+        .isLength({ min: 4 })
+        .withMessage("Password must be at least 4 characters long") 
+];
+
+
+router.post("/register", registerValidation, register);
+router.post("/login", loginValidation, login);
+
+router.get("/profile", protect, getProfile);
+router.put("/profile", protect, updateProfile);
+router.put("/change-password", protect, changePassword);
